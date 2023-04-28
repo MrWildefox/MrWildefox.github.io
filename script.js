7f7d34fe0824aef7eb2ff45bb9aa4ae5cc666367
Bondar Nazar <04-21-344.stud@vntu.vn.ua>
2023-04-28 11:13:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Отримуємо доступ до елементів сторінки
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+
+// Змінюємо стилі елементів
+header.style.backgroundColor = '#fa8e47';
+footer.style.backgroundColor = 'blue';
